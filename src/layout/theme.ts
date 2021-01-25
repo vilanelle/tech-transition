7b13ed2643b4a5ef144a3d3cb6a0084778b280f5
Olga Nowakowska <o.w.nowakowska@gmail.com>
2021-01-25 17:24:15 +0100

    Add font family and color to theme.ts

--- src/layout/theme.ts
+++ src/layout/theme.ts
@@ -3,12 +3,18 @@ import purple from '@material-ui/core/colors/purple';
 import green from '@material-ui/core/colors/green';
 
 export const theme = createMuiTheme({
+  typography: {
+    fontFamily: 'Segoe UI',
+  },
   palette: {
     primary: {
       main: purple[500],
     },
     secondary: {
       main: green[500],
+    },
+    text: {
+      primary: '#222222',
     },
   },
 });
