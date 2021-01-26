5c662a738645a607953df2ad97e79433813c630b
Olga Nowakowska <o.w.nowakowska@gmail.com>
2021-01-26 14:23:44 +0100

    Add breakpoints to theme.ts, change font size for h1 and body1

--- src/layout/theme.ts
+++ src/layout/theme.ts
@@ -1,10 +1,30 @@
 import { createMuiTheme } from '@material-ui/core/styles';
+import createBreakpoints from '@material-ui/core/styles/createBreakpoints';
 import purple from '@material-ui/core/colors/purple';
 import green from '@material-ui/core/colors/green';
 
+const breakpoints = createBreakpoints({});
+
 export const theme = createMuiTheme({
   typography: {
-    fontFamily: 'Segoe UI',
+    fontFamily: 'Segoe UI, sans-serif',
+    h1: {
+      fontSize: 40,
+      [breakpoints.down('sm')]: {
+        fontSize: 30,
+      },
+      [breakpoints.down('xs')]: {
+        fontSize: 24,
+      },
+    },
+    body1: {
+      [breakpoints.down('sm')]: {
+        fontSize: 14,
+      },
+      [breakpoints.down('xs')]: {
+        fontSize: 13,
+      },
+    },
   },
   palette: {
     primary: {
