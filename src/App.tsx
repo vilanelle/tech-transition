830d28917ea90c03bbd2a9dd4310ebeb6c4207a9
Olga Nowakowska <o.w.nowakowska@gmail.com>
2021-01-26 14:37:22 +0100

    Add stylesProvider injectFirst to App to fix owerwritting styles by Material UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,20 @@
 import { ThemeProvider as MuiThemeProvider } from '@material-ui/core';
 import React from 'react';
 import { ThemeProvider } from 'styled-components';
+import { StylesProvider } from '@material-ui/core/styles';
 import { DemoContainer } from './demo/DemoContainer';
 import { theme } from './layout/theme';
 
+import { HomePageLayout } from './layout/HomePageLayout';
+
 export const App: React.FC<{}> = () => (
-  <MuiThemeProvider theme={theme}>
-    <ThemeProvider theme={theme}>
-      {/* Replace with actual content */}
-      <DemoContainer />
-    </ThemeProvider>
-  </MuiThemeProvider>
+  <StylesProvider injectFirst>
+    <MuiThemeProvider theme={theme}>
+      <ThemeProvider theme={theme}>
+        {/* Replace with actual content */}
+        <HomePageLayout />
+        {/* <DemoContainer /> */}
+      </ThemeProvider>
+    </MuiThemeProvider>
+  </StylesProvider>
 );
