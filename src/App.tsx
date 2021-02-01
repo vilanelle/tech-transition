246020e1076f3e430c7249ed237f12b5f4727c43
joannajerz <32146751+joannajerz@users.noreply.github.com>
2021-02-01 12:47:36 +0100

    Drm 12 [Landing page] Routing to Detail Page  (#3)
    * react router dom install
    
    * create routes.tsx, change tsconfig configuration - for using react router dom libraly
    
    * first route
    
    * Routing to detail page
    
    * remove react-router-dom
    
    * code review fixes
    
    * remove .vscode
    
    * Routing fixed and remove yarn
    
    * new package-json.lock

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,32 @@
 import { ThemeProvider as MuiThemeProvider } from '@material-ui/core';
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
 import { StylesProvider } from '@material-ui/core/styles';
 import { HomePage } from './home-page/HomePage';
 import { theme } from './layout/theme';
+import { ITProfessions } from './ITProfessions';
 
 export const App: React.FC<{}> = () => (
   <StylesProvider injectFirst>
     <MuiThemeProvider theme={theme}>
       <ThemeProvider theme={theme}>
-        <HomePage />
+        <Router>
+          <Switch>
+            <Route exact path="/">
+              <HomePage />
+            </Route>
+            {ITProfessions.map((r) => (
+              <Route exact path={`/${r}`} key={r}>
+                <p>Works {r}</p>
+              </Route>
+            ))}
+          </Switch>
+        </Router>
       </ThemeProvider>
     </MuiThemeProvider>
   </StylesProvider>
