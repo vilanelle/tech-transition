449fc2ae10588e9651ab038f250d4f4b4a131148
Olga Nowakowska <o.w.nowakowska@gmail.com>
2021-02-04 17:49:30 +0100

    Create file with tab categories

---
+++ src/detail-page/components/TabCategories.tsx
@@ -0,0 +1,19 @@
+type Category = {
+    name: string;
+    text: string;
+}
+
+export const categories: Category[] = [
+  {
+    name: 'learn',
+    text: 'What do I learn',
+  },
+  {
+    name: 'jobs',
+    text: 'Jobs',
+  },
+  {
+    name: 'resources',
+    text: 'Useful Resources',
+  },
+];
