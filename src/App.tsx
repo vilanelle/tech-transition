a6d1ba9e62c1717de6e683c86dbff65ee3717f56
Anna Mierzwinska <amierzwinska@cybervadis.com>
2021-02-20 18:14:01 +0100

    revert unnecessary changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { ThemeProvider as MuiThemeProvider } from '@material-ui/core';
 import React from 'react';
-import { Redirect, Route, Switch } from 'react-router-dom';
+import { Redirect, Route, Switch, BrowserRouter as Router } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
 import { StylesProvider } from '@material-ui/core/styles';
 import { HomePage } from './home-page/HomePage';
@@ -12,13 +12,15 @@ export const App: React.FC<{}> = () => (
   <StylesProvider injectFirst>
     <MuiThemeProvider theme={theme}>
       <ThemeProvider theme={theme}>
-        <Switch>
-          <Route exact path="/">
-            <HomePage />
-          </Route>
-          <Redirect exact path="/details" to={`/details/${ITProfessions[0].route}`} />
-          <Route path="/details/:professionId" component={DetailPage} />
-        </Switch>
+        <Router>
+          <Switch>
+            <Route exact path="/">
+              <HomePage />
+            </Route>
+            <Redirect exact path="/details" to={`/details/${ITProfessions[0].route}`} />
+            <Route path="/details/:professionId" component={DetailPage} />
+          </Switch>
+        </Router>
       </ThemeProvider>
     </MuiThemeProvider>
   </StylesProvider>
