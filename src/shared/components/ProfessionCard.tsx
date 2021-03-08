88b1ac7b4974bc46e819f1b00504fd7bbfedc2be
Małgorzata Olborska <ireshka@users.noreply.github.com>
2021-03-08 21:09:26 +0100

    DRM-16 - Reuse Card Component & DRM-41 - Extra wrapper in main container (#14)
    * feat: add card component to profession details view
    
    * fix: refactor maincontainter component
    
    * feat: add project apple touch icon
    
    * style: Add space to container and change for using theme spacing
    
    * fix: Rewrite ProfessionCard logic
    
    * chore: Unify spacing with theme values
    
    * fix: isClickable props & props interpolation

---
+++ src/shared/components/ProfessionCard.tsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import { Card, CardHeader, Avatar } from '@material-ui/core';
+import styled from 'styled-components';
+import { ProfessionKey } from '../../data/ITProfessions';
+
+const StyledProfessionCard = styled(Card)<{ $isClickable: boolean }>`
+  width: 100%;
+  display: flex;
+  align-items: center;
+  cursor: ${({ $isClickable }) => ($isClickable ? 'pointer' : 'auto')};
+  :hover {
+    opacity: ${({ $isClickable }) => ($isClickable ? 0.8 : 1)};
+  }
+`;
+
+const StyledCardHeader = styled(CardHeader)`
+  width: 100%;
+`;
+
+const StyledAvatar = styled(Avatar)<{ background: ProfessionKey }>`
+  margin-right: 1em;
+  background-color: ${({ background, theme }) => theme.palette.professions[background].main};
+  padding: 0.35em;
+`;
+
+type ProfessionCardProps = {
+  title: string;
+  description: string;
+  avatarSrc: string;
+  avatarBackground: ProfessionKey;
+  isClickable?: boolean;
+};
+
+export const ProfessionCard: React.FC<ProfessionCardProps> = ({
+  title,
+  description,
+  avatarSrc,
+  avatarBackground,
+  isClickable = false,
+}) => (
+  <StyledProfessionCard $isClickable={isClickable}>
+    <StyledCardHeader title={title} subheader={description} />
+    <StyledAvatar src={avatarSrc} background={avatarBackground} />
+  </StyledProfessionCard>
+);
