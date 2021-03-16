b5bcf7b24544c4ebbe24b8f9e0bb0a2e5de4464f
Anna Mierzwinska <amierzwinska@cybervadis.com>
2021-03-16 17:38:14 +0100

    partial data

---
+++ src/data/useful-resources.ts
@@ -0,0 +1,32 @@
+export const usefulResources = {
+    frontend: {
+        onlineCourses: [
+            {
+                platform: 'freeCodeCamp',
+                urls: ['https://www.freecodecamp.org']
+            },
+            {
+                platform: 'CS50',
+                urls: ['https://www.edx.org/course/cs50s-introduction-to-computer-science']
+            },
+            {
+                platform: 'SheCodes',
+                urls: ['https://www.shecodes.io/']
+            },
+            {
+                platform: 'FrontendMasters',
+                urls: ['https://frontendmasters.com/']
+            },
+        ],
+        learningGroups: [
+            {
+                platform: 'Facebook',
+                urls: ['Programowanie - wsparcie na starcie', 'Od Juniora Do Seniora', 'HTML/CSS/Javascript - Front-end PL', 'DareIT Community']
+            },
+            {
+                name: 'Slack',
+                urls: ['DareIt']
+            },
+        ]
+    }
+};
